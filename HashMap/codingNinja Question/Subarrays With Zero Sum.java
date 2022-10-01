286fac7a1d9e6ac5a0c37186d0e200697d69c740
Hardik Aggarwal <71537080+csehardik@users.noreply.github.com>
2022-10-01 21:33:47 +0530

    Create Subarrays With Zero Sum.java

---
+++ HashMap/codingNinja Question/Subarrays With Zero Sum.java
@@ -0,0 +1,27 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+	public static int countSubarrays(int n, int[] arr) {
+		HashMap<Integer,Integer>memo = new HashMap<Integer,Integer>();
+        
+        int prefixSum=0;
+        int answer = 0;
+        
+        memo.put(prefixSum,1);
+        
+        for(int i = 0;i<n;i++){
+            prefixSum+=arr[i];
+            
+            if(memo.containsKey(prefixSum)){
+                answer+=memo.get(prefixSum);
+            memo.put(prefixSum,memo.get(prefixSum)+1);
+            }else{
+                memo.put(prefixSum,1);
+            }
+        }
+        return answer;
+	}
+
+}
