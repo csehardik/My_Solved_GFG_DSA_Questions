6dae1c5e4617de3c593ff4e3a164990da66799b7
Hardik Aggarwal <71537080+csehardik@users.noreply.github.com>
2022-10-03 12:33:47 +0530

    Update Largest Subarray with 0 sum.java

--- HashMap/Largest Subarray with 0 sum.java
+++ HashMap/Largest Subarray with 0 sum.java
@@ -1,20 +1,38 @@
+//Approach we Used :
+
+// in this hashMap has entry like [key = prefixSum & value = currentIndex]
+
+// if we have two points A & B having n points placed and having sum = S 
+// then we have two points A & C and having point B placed in b/w A & C , B/w A & B there are m points placed  having sum = S
+// so , that means we have sum = 0 B/W points B & C 
+// Sum(AC) = Sum(AB) + Sum(BC) => [ S = S + Sum(BC) ]=> [ Sum(BC) = 0 ]
+
+
 class GfG
 {
     int maxLen(int arr[], int n)
     {
-        HashMap<Integer,Integer>memo= new HashMap<Integer,Integer>();
+        HashMap<Integer,Integer>memo= new HashMap<Integer,Integer>();//initialise a new empty hashMap of Integer type key : value pair
         int answer=0;
         int prefixSum=0;
         
-        memo.put(prefixSum,-1);
+        memo.put(prefixSum,-1); // In the hashMap place value = -1 for key = 0
         
         for(int i=0; i<n;i++){
-            prefixSum+=arr[i];
-        if(memo.containsKey(prefixSum))
-        answer = Math.max(answer,i-memo.get(prefixSum));
+            prefixSum+=arr[i]; // As we traverse through the array add each number we encounter ,  in the prefixSum (i.e 0) 
+            
+        if(memo.containsKey(prefixSum)) // check whether the value we get after adding in the prefixSum is already present in hashMap or not.
+        answer = Math.max(answer,i-memo.get(prefixSum));// if we get the value of prefixSum already present before in the hashMap , 
+                                                       // we replace the value of answer from 0 to max of (answer , longest subarray 
+                                                      // having sum zero(0) , here i - memo.get (prefixSum) means , currentIndex having 
+                                                     // prefixSum already encountered - index on which the encountered value was firstly stored/present)
+        
         else
-        memo.put(prefixSum,i);
+        memo.put(prefixSum,i);// if our memo encounters a new prefixSum value that's not matching any stored value of hashMap then , we will store value of
+                             //prefixSum having currentIndex value i
+        
         }
+        
         return answer;
     }
 }
